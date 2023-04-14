10f63388a584cd28569f429330cbd5bf9ed5d5c3
Łukasz Grolik <lukasz@grolik.pl>
2023-04-14 20:16:21 +0200

    generate-images-size-variants function

--- src/lib/file-metadata/generate-images-size-variants.ts
+++ src/lib/file-metadata/generate-images-size-variants.ts
@@ -4,108 +4,173 @@ import * as sharp from 'sharp';
 import { systemPath, SystemPath } from '../system-path';
 
 // type DestFilenameFn = (name: string, size: [number, number], ext: string, fullName: string) => string;
+type DestFilenameFn = (name: string, size: number, ext: string, fullName: string) => string;
 
-// export interface GenerateSizeVariantsOpts {
-//     files: {
-//         src: SystemPath;
-//         destDir: SystemPath;
-//         destFilename: DestFilenameFn,
-//         sizes: { maxHeight: number }[];
-//     }[];
-//     onFileProcessed?: (info: { src: SystemPath; sizes: { dest: SystemPath; size: [number, number] }[]; count: number; progress: number }) => void;
-//     onFileError?: (info: { src: SystemPath; destDir: SystemPath; error: Error }) => void;
+type GenerateSizeVariantFile = {
+    src: SystemPath;
+    destDir: SystemPath;
+    // destFilename: DestFilenameFn;
+    sizes: {
+        maxHeight: number;
+    }[];
+};
+
+export interface GenerateSizeVariantsOpts {
+    files: GenerateSizeVariantFile[];
+    // onFileProcessed?: (info: { src: SystemPath; sizes: { dest: SystemPath; size: [number, number] }[]; count: number; progress: number }) => void;
+    onFileProcessed?: (info: { src: SystemPath; sizes: { path: SystemPath; size: number }[]; count: number; progress: number }) => void;
+    onFileError?: (info: { src: SystemPath; destDir: SystemPath; error: Error }) => void;
+}
+
+interface GeneratedSizeVariantsProcessedFile {
+    src: SystemPath;
+    sizes: {
+        path: SystemPath;
+        size: number;
+    }[];
+}
+
+interface GenerateSizeVariantsResult {
+    processed: GeneratedSizeVariantsProcessedFile[];
+    failed: FailedOp[];
+}
+
+interface FailedOp {
+    src: SystemPath;
+    destDir: SystemPath;
+    error: Error;
+}
+
+export async function generateImagesSizeVariants(opts: GenerateSizeVariantsOpts): Promise<GenerateSizeVariantsResult> {
+    let processedCount = 0;
+    const processed: GeneratedSizeVariantsProcessedFile[] = [];
+    const failed: FailedOp[] = [];
+
+    const promises = opts.files.map(async file => {
+        let destFiles: GenerateImageSizeVariantsResult[] | undefined = undefined;
+
+        try {
+            destFiles = await generateImageSizeVariants({
+                src: file.src,
+                destDir: file.destDir,
+                // destFilename: file.destFilename,
+                sizes: file.sizes,
+            });
+        }
+        catch (err) {
+            failed.push({
+                src: file.src,
+                destDir: file.destDir,
+                error: err,
+            });
+
+            if (opts.onFileError) {
+                opts.onFileError({
+                    src: file.src,
+                    destDir: file.destDir,
+                    error: err,
+                });
+            }
+        }
+
+        if (destFiles) {
+            // console.log('destFiles', destFiles)
+            processedCount += 1;
+
+            // const sizes = destFiles.map(f => f.size);
+
+            processed.push({
+                src: file.src,
+                sizes: destFiles
+            });
+
+            const progress = processedCount / opts.files.length;
+
+            if (opts.onFileProcessed) {
+                opts.onFileProcessed({
+                    src: file.src,
+                    sizes: destFiles,
+                    count: processedCount,
+                    progress,
+                });
+            }
+        }
+    });
+
+    await Promise.all(promises);
+
+    return {
+        processed,
+        failed,
+    };
+}
+
+type GenerateImageSizeVariantsOpts = {
+    src: SystemPath;
+    destDir: SystemPath;
+    // destFilename: DestFilenameFn;
+    sizes: {
+        maxHeight: number;
+    }[];
+};
+
+type GenerateImageSizeVariantsResult = {
+    path: SystemPath;
+    size: number;
+};
+
+// class BetterPromise<T> extends Promise<T> {
+//     static filter(promises: Promise<T>[]) {
+
+//     }
 // }
 
-// interface GeneratedSizeVariantsProcessedFile {
-//     src: SystemPath;
-//     sizes: {
-//         dest: SystemPath;
-//         size: [number, number];
-//     }[];
-// }
+async function promiseFilter<T>(items: T[], cb: (item: T) => Promise<boolean>): Promise<T[]> {
+    const filtered: T[] = [];
 
-// interface GenerateSizeVariantsResult {
-//     processed: GeneratedSizeVariantsProcessedFile[];
-//     failed: FailedOp[];
-// }
+    const promises = items.map(item => {
+        return cb(item);
+    });
 
-// interface FailedOp {
-//     src: SystemPath;
-//     destDir: SystemPath;
-//     error: Error;
-// }
+    const results = await Promise.all(promises);
+    results.forEach((r, i) => {
+        if (r) filtered.push(items[i]);
+    });
 
-// export async function generateImagesSizeVariants(opts: GenerateSizeVariantsOpts): Promise<GenerateSizeVariantsResult> {
-//     let processedCount = 0;
-//     const processed: GeneratedSizeVariantsProcessedFile[] = [];
-//     const failed: FailedOp[] = [];
+    return filtered;
+}
 
-//     const promises = opts.files.map(async file => {
-//         let destFiles: SystemPath[] | undefined = undefined;
+async function generateImageSizeVariants(opts: GenerateImageSizeVariantsOpts): Promise<GenerateImageSizeVariantsResult[]> {
+    // @todo provide optional callback arg to utilize cached metadata height
+    const validImages = await promiseFilter(opts.sizes, async size => {
+        const metadataOrigin = await sharp(opts.src.path).metadata();
 
-//         try {
-//             destFiles = await generateImageSizeVariants({
-//                 src: file.src,
-//                 destDir: file.destDir,
-//                 destFilename: file.destFilename,
-//                 sizes: file.sizes,
-//             });
-//         }
-//         catch (err) {
-//             failed.push({
-//                 src: file.src,
-//                 destDir: file.destDir,
-//                 error: err,
-//             });
+        // const inputW = 123;
+        const inputH = metadataOrigin.height;
 
-//             if (opts.onFileError) {
-//                 opts.onFileError({
-//                     src: file.src,
-//                     destDir: file.destDir,
-//                     error: err,
-//                 });
-//             }
-//         }
+        if (!inputH) throw new Error(`Image height not found (${opts.src.path})`);
 
-//         if (destFiles) {
-//             processedCount += 1;
+        return inputH > size.maxHeight;
+    });
 
-//             processed.push({
-//                 src: file.src,
-//                 sizes: destFiles
-//             });
+    const promises = validImages.map(async size => {
+        // const ratio = inputW / inputH;
+        // const w = ratio * size.maxHeight;
+        // const h = size.maxHeight;
+        // const outputFileName = opts.destFilename(opts.src.base, size.maxHeight, opts.src.ext, opts.src.file);
+        // const dest = path.join(opts.destDir.path, outputFileName);
+        const dest = systemPath(path.join(opts.destDir.path, `${opts.src.fileExtPartial}_${size.maxHeight}.${opts.src.extLast}`));
 
-//             const progress = processedCount / opts.files.length;
+        await sharp(opts.src.path)
+            .resize({height: size.maxHeight})
+            .toFile(dest.path);
 
-//             if (opts.onFileProcessed) {
-//                 opts.onFileProcessed({
-//                     src: file.src,
-//                     sizes: destFiles,
-//                     count: processedCount,
-//                     progress,
-//                 });
-//             }
-//         }
-//     });
+        return {
+            path: dest,
+            size: size.maxHeight
+        };
+    });
 
-//     await Promise.all(promises);
-
-//     return {
-//         processed,
-//         failed,
-//     };
-// }
-
-// async function generateImageSizeVariants(opts: { src: SystemPath; destDir: SystemPath; destFilename: DestFilenameFn; sizes: [number, number][] }): Promise<SystemPath[]> {
-//     const promises = opts.sizes.map(async size => {
-//         const dest = path.join(opts.destDir.path, opts.destFilename(opts.src.base, [1, 2], opts.src.ext, opts.src.file));
-
-//         await sharp(opts.src.path)
-//             .resize()
-//             .toFile(dest);
-//     });
-
-//     await Promise.all(promises);
-
-//     return systemPath();
-// }
+    // @todo promise-parallel-max or enqueue
+    return Promise.all(promises);
+}
--- src/lib/file-metadata/test/generate-images-size-variants.test.ts
+++ src/lib/file-metadata/test/generate-images-size-variants.test.ts
@@ -3,73 +3,146 @@ import * as fs from 'fs';
 import 'mocha';
 import * as should from 'should';
 import * as sharp from 'sharp';
+
+import { removeDirContents } from '../../utils';
 import { generateImagesSizeVariants, GenerateSizeVariantsOpts } from '../generate-images-size-variants';
 import { systemPath } from '../../system-path';
 import { generateImageSizeVariants } from '../../../generate-thumbnails/generate-image-size-variants';
 
+async function generateThumbnails(body: {paths: string[]; sizes: number[]}) {
+    const opts: GenerateSizeVariantsOpts = {
+        files: body.paths.map(file => {
+            return {
+                src: systemPath(path.resolve(__dirname, 'source-files', file)),
+                destDir: systemPath(path.resolve(__dirname, 'output-files/')),
+                // destFilename: (name, size, ext) => `${name}_${size[1]}${ext}`,
+                sizes: body.sizes.map(size => {
+                    return { maxHeight: size };
+                }),
+            }
+        })
+    };
+
+    const res = await generateImagesSizeVariants(opts);
+
+    should(res.failed.length).equal(0);
+    should(res.processed.length).equal(body.paths.length);
+
+    for (const path of body.paths) {
+        const i = body.paths.indexOf(path);
+        const processedSizes = res.processed[i].sizes;
+
+        const metadataOrigin = await sharp(opts.files[i].src.path).metadata();
+        const metadataOriginHeight = metadataOrigin.height || 0;
+        const sizeRatio = (metadataOrigin.width || 0) / metadataOriginHeight;
+
+        let expectedProcessedSizesCount = 0;
+        for (const size of body.sizes) {
+            if (metadataOriginHeight > size) expectedProcessedSizesCount += 1;
+        }
+
+        // console.log(processedSizes.map(s => s.size));
+
+        // should(processedSizes.length).equal(body.sizes.length);
+        should(processedSizes.length).equal(expectedProcessedSizesCount);
+
+        for (const procSize of processedSizes) {
+            const size = procSize.size;
+
+            // console.log('size', size)
+
+            const metadataResized = await sharp(procSize.path.path).metadata();
+
+            should(metadataResized.height).equal(size);
+            should(metadataResized.width).equal(Math.round(size * sizeRatio));
+        }
+    }
+}
+
 describe('generateSizeVariants', () => {
+    afterEach(async () => {
+        await removeDirContents(path.resolve(__dirname, 'output-files'));
+    });
+
     it('throws if files array contains file with empty src');
     it('throws if files array contains file with empty dest');
     it('throws if files array contains file with empty sizes array');
 
     it('throws if file not found');
 
-    it('generates thumbnail size variants for a jpg image', async () => {
-        const opts: GenerateSizeVariantsOpts = {
-            files: [
-                {
-                    src: systemPath(path.resolve(__dirname, 'source-files/test_1.jpg')),
-                    destDir: systemPath(path.resolve(__dirname, 'output-files/')),
-                    destFilename: (name, size, ext) => `${name}_${size[1]}${ext}`,
-                    sizes: [{ maxHeight: 100 }],
-                }
+    it('generates thumbnail size variant for a jpg image', async () => {
+        await generateThumbnails({
+            paths: [
+                'sunset.jpg'
             ],
-        };
-
-        const metadataOrigin = await sharp(opts.files[0].src.path).metadata();
-        const sizeRatio = (metadataOrigin.width || 0) / (metadataOrigin.height || 0);
-
-        const res = await generateImagesSizeVariants(opts);
-
-        should(res.processed.length).equal(1);
-        should(res.processed[0].sizes.length).equal(1);
-        should(res.processed[0].sizes[0].size[0]).equal(100 * sizeRatio);
-        should(res.processed[0].sizes[0].size[1]).equal(100);
-
-        const metadataResized = await sharp(res.processed[0].sizes[0].dest.path).metadata();
-
-        should(metadataResized.height).equal(100);
-        should(metadataResized.width).equal(100 * sizeRatio);
+            sizes: [360],
+        });
     });
 
-    it('generates thumbnail size variants for a png image');
-    it('generates thumbnail size variants for a gif image');
-    it('generates thumbnail size variants for a webp image');
-
-    it('generates multiple size variants for multiple image types');
-
-    it('does not generate thumbnails larger than source image', async () => {
-        const opts: GenerateSizeVariantsOpts = {
-            files: [
-                {
-                    src: systemPath(path.resolve(__dirname, 'source-files/test_2.jpg')),
-                    destDir: systemPath(path.resolve(__dirname, 'output-files/')),
-                    destFilename: (name, size, ext) => `${name}_${size[1]}${ext}`,
-                    sizes: [{ maxHeight: 100 }],
-                }
+    it('generates thumbnail size variant for a png image', async () => {
+        await generateThumbnails({
+            paths: [
+                'europe.png'
             ],
-        };
-
-        const metadataOrigin = await sharp(opts.files[0].src.path).metadata();
-
-        const res = await generateImageSizeVariants(opts);
-
-        // const metadataCopy = await sharp(opts.files[0].dest).metadata();
-
-        // @todo should not exist
-        await fs.promises.stat(res.processed[0].sizes[0].dest.path);
-
-        // should(metadataCopy.height).equal(metadataOrigin.height);
-        // should(metadataCopy.width).equal(metadataOrigin.width);
+            sizes: [360],
+        });
     });
+
+    it('generates thumbnail size variant for a gif image');
+    it('generates thumbnail size variant for a webp image');
+
+    it('generates multiple thumbnail size variants', async () => {
+        await generateThumbnails({
+            paths: [
+                'sunset.jpg',
+            ],
+            sizes: [180, 360, 720],
+        });
+    });
+
+    it('generates multiple thumbnail size variants for multiple images of the same type', async () => {
+        await generateThumbnails({
+            paths: [
+                'sunset.jpg',
+                'meadow.jpg',
+            ],
+            sizes: [180, 360, 720],
+        });
+    });
+
+    it('generates multiple thumbnail size variants for multiple image types', async () => {
+        await generateThumbnails({
+            paths: [
+                'sunset.jpg',
+                'europe.png',
+            ],
+            sizes: [180, 360, 720],
+        });
+    });
+
+    // it('does not generate thumbnails larger than source image', async () => {
+    //     const opts: GenerateSizeVariantsOpts = {
+    //         files: [
+    //             {
+    //                 src: systemPath(path.resolve(__dirname, 'source-files/test_2.jpg')),
+    //                 destDir: systemPath(path.resolve(__dirname, 'output-files/')),
+    //                 // destFilename: (name, size, ext) => `${name}_${size[1]}${ext}`,
+    //                 sizes: [{ maxHeight: 100 }],
+    //             }
+    //         ],
+    //     };
+
+    //     const metadataOrigin = await sharp(opts.files[0].src.path).metadata();
+
+    //     const res = await generateImageSizeVariants(opts);
+
+    //     // const metadataCopy = await sharp(opts.files[0].dest).metadata();
+
+    //     // @todo should not exist
+    //     await fs.promises.stat(res.processed[0].sizes[0].dest.path);
+
+    //     // should(metadataCopy.height).equal(metadataOrigin.height);
+    //     // should(metadataCopy.width).equal(metadataOrigin.width);
+    // });
 });
+
